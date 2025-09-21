b10d17e19f9e68837fed0a62b8b9e9fb79bd1abd
sirpent5 <143552447+sirpent5@users.noreply.github.com>
2025-09-21 05:33:50 -0400

    FISH ANIMATION

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 
-import logo from '../assets/StreamLineLogo-removebg-preview.png';
+import logo from '../assets/StreamLineLogo.png';
 
 const Header = () => {
   return (
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/StreamLineLogo-removebg-preview.png';
-import './HomePage.css';
+import logo from '../assets/rightFacingFish.png'; // 👈 The variable is named 'logo'
+import Finallogo from '../assets/StreamLineLogo.png';
+import './Home.css';
 import BubblesAnimation from '../components/BubblesAnimation.jsx';
 
 const HomePage = () => {
   return (
-    <div className="home-container">
-      <Link to="/app" className="logo-link">
-        <img src={logo} alt="Project Logo" />
-        <p>Click fish to StreamLine!</p>
-      </Link>
-      <BubblesAnimation /> {/* Add the bubbles component here */}
+    <div>
+      {/* Container for the fish */}
+      {/* Use the correct variable name 'logo' here */}
+      <img src={logo} alt="right-facing fish" className="fish" />
+      <img src={Finallogo} alt="Normal Fish" className="finalFish" />
+
+      {/* Containers for the bubbles */}
+      <div className="bubbles"></div>
+      <div className="bubbles bubble-2"></div>
+      <div className="bubbles bubble-3"></div>
+      <div className="bubbles bubble-4"></div>
+      <div className="bubbles bubble-5"></div>
+      <div className="bubbles bubble-6"></div>
+      <div className="bubbles bubble-7"></div>
+      <div className="bubbles bubble-8"></div>
     </div>
   );
 };
