4f55cdaaf7ac384df12cc34129c94329a7dea49e
ChristineG29 <cglascott29@gmail.com>
2025-09-21 10:27:14 -0400

    Updating About

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,13 +1,23 @@
-// frontend/src/pages/AboutPage.jsx
 import React from 'react';
+import './AboutPage.css';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="about-container">
       <h1>About Us</h1>
       <p>Hi! We are Christine, Mia, and Tess aka team Mascot Wig!!</p>
-      <p>This project was made for SteelHacks XII track for "Product Design" and awards for "Best Use of the Cloud" and "Best Beginner."</p>
-      <p>We designed this application with students in mind to advise the best possible subscription plan based on their favorite genres, budget, and more.</p>
+      <p>
+        We are all students at the University of Pittsburgh studying Computer and Information Science. 
+      </p>
+      <p>
+        This project was made for SteelHacks XII track for "Product Design" and
+        awards for "Best Use of the Cloud" and "Best Beginner."
+      </p>
+      <p>
+        We designed this application with students in mind to advise the best
+        possible subscription plan based on their favorite genres, budget, and
+        more.
+      </p>
     </div>
   );
 };
