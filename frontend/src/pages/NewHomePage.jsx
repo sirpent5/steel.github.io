323913af5b280d6aa5f73af4c76a04d50f14342e
ChristineG29 <cglascott29@gmail.com>
2025-09-21 10:19:32 -0400

    Updating Home Pages

--- frontend/src/pages/NewHomePage.jsx
+++ frontend/src/pages/NewHomePage.jsx
@@ -1,11 +1,15 @@
-// frontend/src/pages/NewHomePage.jsx
 import React from 'react';
+import './NewHomePage.css';
 
 const NewHomePage = () => {
   return (
-    <div>
-      <h1>Welcome Back to the Homepage!</h1>
-      <p>This is your new homepage content.</p>
+    <div className="new-home-container">
+      <div className="overlay">
+        <h1 className="welcome-text">Welcome to StreamLine</h1>
+        <p className="sub-text">Your hub for evaluating and comparing streaming services.</p>
+        <p className="sub-text">Head to the evaluate tab to find a plan for you!</p>
+        <p className="sub-text">Enter two services of your choice and calculate your overlap to get recommendations on how you can save money by choosing one over the other!</p>
+      </div>
     </div>
   );
 };
